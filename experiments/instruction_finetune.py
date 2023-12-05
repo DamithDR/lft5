9a405673272b3c44034069bfd36db87e777ee6d4
Damith Premasiri <damithpremasiri@gmail.com>
2023-12-05 22:33:33 +0000

    initial commit

--- experiments/instruction_finetune.py
+++ experiments/instruction_finetune.py
@@ -39,16 +39,16 @@ def run(args):
     dataset = pd.read_csv(f'data/permuted_data/{args.dataset_file_name}')
     data = Dataset.from_pandas(dataset)
 
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16
-    )
+    # bnb_config = BitsAndBytesConfig(
+    #     load_in_4bit=True,
+    #     bnb_4bit_use_double_quant=True,
+    #     bnb_4bit_quant_type="nf4",
+    #     bnb_4bit_compute_dtype=torch.bfloat16
+    # )
 
     model = AutoModelForCausalLM.from_pretrained(
         args.model_name,
-        quantization_config=bnb_config,
+        # quantization_config=bnb_config,
         #     load_in_8bit=True,  #if you want to load the 8-bit model
         #     device_map='auto',
         device_map="auto",
