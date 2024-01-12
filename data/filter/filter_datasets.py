a0ad59433553e7699ee2b583e7c41ed9f8b96640
Damith Premasiri <damithpremasiri@gmail.com>
2024-01-12 21:57:09 +0000

    data filter with stream

--- data/filter/filter_datasets.py
+++ data/filter/filter_datasets.py
@@ -14,11 +14,20 @@ else:
     dataset = load_dataset('joelito/Multi_Legal_Pile', config, split='train', streaming=True)
 
 filtered_list = []
-
+part_number = 1
+count = 0
+removed = 0
 for data in dataset:
     if len(data['text'].split(" ")) <= 1024:
+        count += 1
         filtered_list.append(data['text'])
-
-df = pd.DataFrame()
-df['text'] = filtered_list
-df.to_csv('en_all_filtered_1024.tsv', sep='\t', index=False)
+        if len(filtered_list) == 500000:
+            df = pd.DataFrame()
+            df['text'] = filtered_list
+            df.to_csv(f'en_all_filtered_1024_part_{part_number}.tsv', sep='\t', index=False)
+            part_number += 1
+            filtered_list = []
+        print(f'processing part number : {part_number} | count number : {count}')
+    else:
+        removed += 1
+print(f'used count {count} | removed count {removed}')
