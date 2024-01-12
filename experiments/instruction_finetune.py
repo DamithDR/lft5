a534c9f7f086424421911ccb17e09bab9a54ac76
Damith Premasiri <damithpremasiri@gmail.com>
2024-01-12 19:59:00 +0000

    change device

--- experiments/instruction_finetune.py
+++ experiments/instruction_finetune.py
@@ -65,7 +65,7 @@ def run(args):
         args.model_name,
         quantization_config=bnb_config,
         device_map="auto",
-        # max_memory={0: args.max_mem, 1: args.max_mem, 2: args.max_mem, 3: args.max_mem, "cpu": "120GiB"},
+        max_memory={0: "20GIB", 1: "20GIB"},
         offload_folder="offload", offload_state_dict=True,
         trust_remote_code=True,
     )
