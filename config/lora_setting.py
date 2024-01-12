0d6a64a612517e3fd23de3ca2fb89b9a71763176
Damith Premasiri <damithpremasiri@gmail.com>
2024-01-12 19:43:10 +0000

    100

--- config/lora_setting.py
+++ config/lora_setting.py
@@ -1,11 +1,6 @@
 CONFIG = {
-    'falcon': {
-        'target_modules': ["query_key_value"]
-    },
-    'llama': {
-        'target_modules': ["q_proj", "k_proj", "v_proj", "o_proj"]
-    },
-    'mistral': {
-        'target_modules': ["q_proj", "k_proj", "v_proj", "o_proj"]
-    }
+    'falcon': ["query_key_value"],
+    'llama': ["q_proj", "k_proj", "v_proj", "o_proj"],
+    'mistral': ["q_proj", "k_proj", "v_proj", "o_proj"]
+
 }
